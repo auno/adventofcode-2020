014e601cfecaedfdce2b7e4900102b637a1f1160
Mikael Auno <mikael@auno.se>
2020-12-01 11:31:29 +0100

    Finished 2020/01b

---
+++ 01b/src/main.rs
@@ -0,0 +1,35 @@
+use std::io::{self, BufRead};
+
+fn find_sum(nums: &[i32], target: i32) -> i32 {
+    for a in nums.iter() {
+        for b in nums.iter() {
+            if a + b > target {
+                continue;
+            }
+
+            for c in nums.iter().rev() {
+                if a + b + c == target {
+                    return a * b * c;
+                }
+
+                if a + b + c < target {
+                    break;
+                }
+            }
+        }
+    }
+
+    return -1;
+}
+
+fn main() {
+    let mut nums: Vec<i32> = io::stdin().lock().lines()
+        .map(|line| line.unwrap())
+        .map(|line| line.parse().unwrap())
+        .collect();
+
+    nums.sort();
+    let nums = nums;
+
+    println!("{}", find_sum(&nums, 2020));
+}
